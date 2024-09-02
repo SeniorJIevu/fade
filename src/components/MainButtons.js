ee03a061a519ccba7b3f5b3486cc41c658e8e0c0
SergeyKopko <133692248+SergeyKopko@users.noreply.github.com>
2024-09-02 21:59:23 +0300

    Init commit

--- src/components/MainButtons.js
+++ src/components/MainButtons.js
@@ -3,22 +3,45 @@ import icon_1 from '../assets/buttons/icon_1.png';
 import icon_2 from '../assets/buttons/icon_2.png';
 import icon_3 from '../assets/buttons/icon_3.png';
 import icon_4 from '../assets/buttons/icon_4.png';
+import icon_5 from '../assets/buttons/icon_5.png';
 
 const buttons = [
   { id: 1, text: 'Telegram', icon: icon_4, href: 'https://t.me/fadewallet' },
   { id: 2, text: 'Telegram Chat', icon: icon_3, href: 'https://t.me/fadewalletchat' },
   { id: 3, text: 'Buy token on Dedust', icon: icon_2, href: 'https://dedust.io/swap/TON/EQCh9DUkmEdz7BuM_kv_iDqXeXWHrMUZHcZfXnr8yBAfPHcn' },
   { id: 4, text: '', icon: icon_1, href: 'https://x.com/FadeWallet' },
-  { id: 5, text: 'CA', href: '#' }
+  { id: 5, text: 'CA', href: '#' },
+  { id: 6, text: '', icon: icon_5, href: 'https://www.dextools.io/app/en/ton/pair-explorer/EQBbX-l5f02S1Jka72dM9oQXq70jk-cFQp_gwI5lFXLK_kOk?t=1725302135647' }
 ];
 
-const MainButtons = () => {
+const MainButtons = React.memo(() => {
   const [copied, setCopied] = useState(false);
 
   const handleCopy = () => {
-    navigator.clipboard.writeText('EQCh9DUkmEdz7BuM_kv_iDqXeXWHrMUZHcZfXnr8yBAfPHcn');
-    setCopied(true);
-    setTimeout(() => setCopied(false), 1000);
+    const contractAddress = 'EQCh9DUkmEdz7BuM_kv_iDqXeXWHrMUZHcZfXnr8yBAfPHcn';
+
+    if (navigator.clipboard && navigator.clipboard.writeText) {
+      navigator.clipboard.writeText(contractAddress).then(() => {
+        setCopied(true);
+        setTimeout(() => setCopied(false), 1000);
+      }).catch((err) => {
+        console.error('Failed to copy text: ', err);
+      });
+    } else {
+      const textArea = document.createElement('textarea');
+      textArea.value = contractAddress;
+      document.body.appendChild(textArea);
+      textArea.focus();
+      textArea.select();
+      try {
+        document.execCommand('copy');
+        setCopied(true);
+        setTimeout(() => setCopied(false), 1000);
+      } catch (err) {
+        console.error('Fallback: Oops, unable to copy', err);
+      }
+      document.body.removeChild(textArea);
+    }
   };
 
   return (
@@ -32,13 +55,17 @@ const MainButtons = () => {
           >
             <span className='text-[1.6vh]'>{copied && button.text === 'CA' ? 'Copied' : button.text}</span>
             {button.icon && (
-              <img src={button.icon} alt={button.text} className="ml-[6px] w-[15px] h-[15px]" />
+              <img
+                src={button.icon}
+                alt={button.text}
+                className={`ml-[6px] object-contain ${index === buttons.length - 1 ? 'w-[75px] h-[75px]' : 'w-[15px] h-[15px]'}`}
+              />
             )}
           </a>
         </div>
       ))}
     </div>
   );
-};
+});
 
 export default MainButtons;
